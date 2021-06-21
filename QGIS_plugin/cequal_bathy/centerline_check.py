fc26ea36b3293321331befcbb34bf1b98db8ed2d
jrutyna <jrutyna@limno.com>
2021-06-21 14:04:05 -0400

    QGIS Plugin for CEQUAL Bathy Preprocessor

---
+++ QGIS_plugin/cequal_bathy/centerline_check.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+from qgis.core import QgsProject
+
+def centerlineCheck_func(self, segmentLayer, transectLayer, centerlineLayer):
+    import processing
+    root = QgsProject.instance().layerTreeRoot()
+
+    #Split Centerline at previously calculated W2 transects
+    params = {
+        'INPUT': centerlineLayer,
+        'LINES': transectLayer,
+        'OUTPUT': 'TEMPORARY_OUTPUT' }
+    result_splitCenterline = processing.run("native:splitwithlines", params)
+    result_layer_splitCenterline = result_splitCenterline['OUTPUT']
+
+    #Match with W2 segment IDs
+    params = {
+        'DISCARD_NONMATCHING' : False,
+        'INPUT' : result_layer_splitCenterline,
+        'JOIN' : segmentLayer,
+        'JOIN_FIELDS' : [],
+        'METHOD' : 1,
+        'OUTPUT' : 'TEMPORARY_OUTPUT',
+        'PREDICATE' : [0],
+        'PREFIX' : 'sgmt_' }
+    result_splitCenterlineJoin = processing.run("native:joinattributesbylocation", params)
+    result_layer_splitCenterlineJoin = result_splitCenterlineJoin['OUTPUT']
+
+    #Intersection of split centerlines and W2 polygon segments
+    params = {
+        'INPUT' : result_layer_splitCenterlineJoin,
+        'INPUT_FIELDS' : [],
+        'OUTPUT' : 'TEMPORARY_OUTPUT',
+        'OVERLAY' : segmentLayer,
+        'OVERLAY_FIELDS' : [],
+        'OVERLAY_FIELDS_PREFIX' : '' }
+    result_splitCenterlineJoinIntrct = processing.run("native:intersection", params)
+    result_layer_splitCenterlineJoinIntrct = result_splitCenterlineJoinIntrct['OUTPUT']
+
+    #Assign Geometry Attributes to Split Centerlines
+    params = {
+        'CALC_METHOD': 0,
+        'INPUT': result_layer_splitCenterlineJoin,
+        'OUTPUT': 'TEMPORARY_OUTPUT'}
+    result_splitCenterlineJoinAttr = processing.run("qgis:exportaddgeometrycolumns", params)
+    result_layer_splitCenterlineJoinAttr = result_splitCenterlineJoinAttr['OUTPUT']
+
+    #Assign Geometry Attributes to Split Centerlines that have been "intersected" with the W2 segments
+    params = {
+        'CALC_METHOD': 0,
+        'INPUT': result_layer_splitCenterlineJoinIntrct,
+        'OUTPUT': 'TEMPORARY_OUTPUT'}
+    result_splitCenterlineJoinIntrctAttr = processing.run("qgis:exportaddgeometrycolumns", params)
+    result_layer_splitCenterlineJoinIntrctAttr = result_splitCenterlineJoinIntrctAttr['OUTPUT']
+
+    #Spatial Join with the W2 segments to Split Centerlines
+    params = {
+        'DISCARD_NONMATCHING' : False,
+        'INPUT' : segmentLayer,
+        'JOIN' : result_layer_splitCenterlineJoinAttr,
+        'JOIN_FIELDS' : [],
+        'METHOD' : 2,
+        'OUTPUT' : 'TEMPORARY_OUTPUT',
+        'PREDICATE' : [0],
+        'PREFIX' : 'splt_' }
+    result_Segments_Split = processing.run("native:joinattributesbylocation", params)
+    result_layer_Segments_Split = result_Segments_Split['OUTPUT']
+
+    #Spatial Join with the W2 segments to Split Centerlines that have been "intersected"
+    params = {
+        'DISCARD_NONMATCHING' : False,
+        'INPUT' : segmentLayer,
+        'JOIN' : result_layer_splitCenterlineJoinIntrctAttr,
+        'JOIN_FIELDS' : [],
+        'METHOD' : 2,
+        'OUTPUT' : 'TEMPORARY_OUTPUT',
+        'PREDICATE' : [0],
+        'PREFIX' : 'spltInt_' }
+    result_Segments_SplitIntrct = processing.run("native:joinattributesbylocation", params)
+    result_layer_Segments_SplitIntrct = result_Segments_SplitIntrct['OUTPUT']
+
+    #Perform Table Join between W2 segements Split and W2 segements Split that have been "intersected"
+    params = {
+        'DISCARD_NONMATCHING': False,
+        'FIELD': 'order_id',
+        'FIELDS_TO_COPY': [],
+        'FIELD_2': 'order_id',
+        'INPUT': result_layer_Segments_Split,
+        'INPUT_2': result_layer_Segments_SplitIntrct,
+        'METHOD': 1,
+        'OUTPUT': 'TEMPORARY_OUTPUT',
+        'PREFIX': ''}
+    result_Segments_joined = processing.run("native:joinattributestable", params)
+    result_layer_Segments_joined = result_Segments_joined['OUTPUT']
+
+    #Create field for centerline all inside W2 segment check
+    params = {
+        'FIELD_LENGTH': 10,
+        'FIELD_NAME': 'Is_CL_In',
+        'FIELD_PRECISION': 10,
+        'FIELD_TYPE': 1,
+        'FORMULA': 'if( round( \"splt_length\", 4) - round( \"spltInt_length\", 4) = 0, 1, 0)',
+        'INPUT': result_layer_Segments_joined,
+        'OUTPUT': 'TEMPORARY_OUTPUT'}
+    result_Segments_joined_chck = processing.run("native:fieldcalculator", params)
+    result_layer_Segments_joined_chck = result_Segments_joined_chck['OUTPUT']
+
+    #Remove unwanted fields
+    params = {
+        'INPUT': result_layer_Segments_joined_chck,
+        'COLUMN': ["splt_Name",
+                   "splt_sgmt_order_id",
+                   "splt_sgmt_IsConvex",
+                   "splt_length",
+                   "order_id_2",
+                   "IsConvex_2",
+                   "spltInt_Name",
+                   "spltInt_sgmt_order_id",
+                   "spltInt_sgmt_IsConvex",
+                   "spltInt_order_id",
+                   "spltInt_IsConvex",
+                   "spltInt_length"],
+        'OUTPUT': 'TEMPORARY_OUTPUT'}
+    result_Segments_joined_chck_rmv = processing.run("qgis:deletecolumn", params)
+    result_layer_Segments_joined_chck_rmv = result_Segments_joined_chck_rmv['OUTPUT']
+
+    #Remove existing Segments Layer and Add Updated Segment Layer with Centerline Check Attribute
+    for lyr in QgsProject.instance().mapLayers().values():
+        if lyr.name() == "Segments":
+            QgsProject.instance().removeMapLayers([lyr.id()])
+    result_layer_Segments_joined_chck_rmv.setName("Segments")
+    QgsProject.instance().addMapLayer(result_layer_Segments_joined_chck_rmv)
+
+
---
+++ QGIS_plugin/cequal_bathy/create_poly.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+from qgis.core import QgsProject
+
+def createPolygon_func(self, selectedLayer, seg_ct, transectWidth):
+    import processing
+    root = QgsProject.instance().layerTreeRoot()
+
+    extendWidth = transectWidth / 2
+
+    #get active layer
+    iface = self.iface
+    alyr=iface.activeLayer()
+
+    #Get length of river centerline
+    #params = { 'CALC_METHOD' : 0, 'INPUT' : QgsProcessingFeatureSourceDefinition('G:/2ERDC02/GIS/Demo_Files_31Mar2021/NapaRiver_CL.shp'), 'OUTPUT' : 'TEMPORARY_OUTPUT' }
+    params = { 'CALC_METHOD' : 0, 'INPUT' : selectedLayer, 'OUTPUT' : 'TEMPORARY_OUTPUT' }
+    result_len = processing.run("qgis:exportaddgeometrycolumns", params)
+    result_layer1 = result_len['OUTPUT']
+    len=0
+    for feature in result_layer1.getFeatures():
+        len += feature["length"]
+
+    #calc segment length
+    seg_len = round(len/seg_ct,4)
+
+    #Get points with angle
+    #params = { 'DISTANCE' : seg_len, 'END_OFFSET' : 0, 'INPUT' : QgsProcessingFeatureSourceDefinition('G:/2ERDC02/GIS/Demo_Files_31Mar2021/NapaRiver_CL.shp'), 'OUTPUT' : 'TEMPORARY_OUTPUT', 'START_OFFSET' : 0 }
+    params = { 'DISTANCE' : seg_len, 'END_OFFSET' : 0, 'INPUT' : selectedLayer, 'OUTPUT' : 'TEMPORARY_OUTPUT', 'START_OFFSET' : 0 }
+    result_pts = processing.run("native:pointsalonglines", params)
+    result_layer_pts = result_pts['OUTPUT']
+    #QgsProject.instance().addMapLayer(result_layer_pts)
+
+    #Extend and rotate lines
+    params ={ 'EXPRESSION' : 'extend(\r\n make_line(\r\n $geometry,\r\n project (\r\n $geometry, \r\n '+str(extendWidth) +', \r\n radians(\"angle\"-90))\r\n ),\r\n '+str(extendWidth) +',\r\n 0\r\n)', 'INPUT' : result_layer_pts, 'OUTPUT' : 'TEMPORARY_OUTPUT', 'OUTPUT_GEOMETRY' : 1, 'WITH_M' : False, 'WITH_Z' : False }
+    result_rot = processing.run("native:geometrybyexpression", params)
+    result_layer_rot = result_rot['OUTPUT']
+
+    #populate order id
+    params={ 'FIELD_LENGTH' : 10, 'FIELD_NAME' : 'order_id', 'FIELD_PRECISION' : 4, 'FIELD_TYPE' : 1, 'INPUT' : result_layer_rot, 'OUTPUT' : 'TEMPORARY_OUTPUT' }
+    result_rot = processing.run("native:addfieldtoattributestable", params)
+    result_layer_rot = result_rot['OUTPUT']
+    params={ 'FIELD_LENGTH' : 0, 'FIELD_NAME' : 'order_id', 'FIELD_PRECISION' : 0, 'FIELD_TYPE' : 1, 'FORMULA' : '@row_number', 'INPUT' : result_layer_rot, 'OUTPUT' : 'TEMPORARY_OUTPUT' }
+    result_rot = processing.run("native:fieldcalculator", params)
+    result_layer_rot = result_rot['OUTPUT']
+    #remove existing Transects Layer
+    for lyr in QgsProject.instance().mapLayers().values():
+        if lyr.name() == "Transects":
+            QgsProject.instance().removeMapLayers([lyr.id()])
+    result_layer_rot.setName("Transects")
+    QgsProject.instance().addMapLayer(result_layer_rot)
+
+    #Get starting points
+    params = { 'INPUT' : result_layer_rot, 'OUTPUT' : 'TEMPORARY_OUTPUT', 'VERTICES' : '0' }
+    result_sp = processing.run("native:extractspecificvertices", params)
+    result_layer_sp = result_sp['OUTPUT']
+    #QgsProject.instance().addMapLayer(result_layer_sp)
+
+    #Get end points
+    params = { 'INPUT' : result_layer_rot, 'OUTPUT' : 'TEMPORARY_OUTPUT', 'VERTICES' : '-1' }
+    result_ep = processing.run("native:extractspecificvertices", params)
+    result_layer_ep = result_ep['OUTPUT']
+    #QgsProject.instance().addMapLayer(result_layer_ep)
+
+    #connect paths
+    params = { 'CLOSE_PATH' : False, 'GROUP_EXPRESSION' : '', 'INPUT' : result_layer_sp, 'NATURAL_SORT' : False, 'ORDER_EXPRESSION' : '', 'OUTPUT' : 'TEMPORARY_OUTPUT' }
+    result_p1 = processing.run("native:pointstopath", params)
+    result_layer_p1 = result_p1['OUTPUT']
+    #QgsProject.instance().addMapLayer(result_layer_p1)
+
+    params = { 'CLOSE_PATH' : False, 'GROUP_EXPRESSION' : '', 'INPUT' : result_layer_ep, 'NATURAL_SORT' : False, 'ORDER_EXPRESSION' : '', 'OUTPUT' : 'TEMPORARY_OUTPUT' }
+    result_p2 = processing.run("native:pointstopath", params)
+    result_layer_p2 = result_p2['OUTPUT']
+    #QgsProject.instance().addMapLayer(result_layer_p2)
+
+    #merge paths
+    params={ 'CRS' : None, 'LAYERS' : [result_layer_rot,result_layer_p1,result_layer_p2], 'OUTPUT' : 'TEMPORARY_OUTPUT' }
+    result_lines = processing.run("native:mergevectorlayers", params)
+    result_layer_lines = result_lines['OUTPUT']
+    #QgsProject.instance().addMapLayer(result_layer_lines)
+
+    #polygonize
+    params={ 'INPUT' : result_layer_lines, 'KEEP_FIELDS' : False, 'OUTPUT' : 'TEMPORARY_OUTPUT' }
+    result_poly = processing.run("native:polygonize", params)
+    result_layer_poly = result_poly['OUTPUT']
+    #QgsProject.instance().addMapLayer(result_layer_poly)
+
+    #add number ID field
+    params={ 'FIELD_LENGTH' : 10, 'FIELD_NAME' : 'order_id', 'FIELD_PRECISION' : 4, 'FIELD_TYPE' : 1, 'INPUT' : result_layer_poly, 'OUTPUT' : 'TEMPORARY_OUTPUT' }
+    result_poly = processing.run("native:addfieldtoattributestable", params)
+    result_layer_poly = result_poly['OUTPUT']
+    #QgsProject.instance().addMapLayer(result_layer_poly)
+
+    #populate order id
+    params={ 'FIELD_LENGTH' : 0, 'FIELD_NAME' : 'order_id', 'FIELD_PRECISION' : 0, 'FIELD_TYPE' : 1, 'FORMULA' : '@row_number', 'INPUT' : result_layer_poly, 'OUTPUT' : 'TEMPORARY_OUTPUT' }
+    result_poly = processing.run("native:fieldcalculator", params)
+    result_layer_poly = result_poly['OUTPUT']
+    #remove existing Segments Layer
+    for lyr in QgsProject.instance().mapLayers().values():
+        if lyr.name() == "Segments":
+            QgsProject.instance().removeMapLayers([lyr.id()])
+    result_layer_poly.setName("Segments")
+    QgsProject.instance().addMapLayer(result_layer_poly)
+
+    #run convex check function
+    segmentLayer = QgsProject.instance().mapLayersByName('Segments')[0]
+    self.convexCheck(segmentLayer)
+
+    #run centerline check function
+    segmentLayer = QgsProject.instance().mapLayersByName('Segments')[0]
+    transectLayer = QgsProject.instance().mapLayersByName('Transects')[0]
+    centerlineLayer = selectedLayer
+    self.centerlineCheck(segmentLayer, transectLayer, centerlineLayer)
